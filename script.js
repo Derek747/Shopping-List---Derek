caf083d2e586d439ff37e98682bbaa3b2bdd95ab
Derek <iamgoatli@gmail.com>
2020-03-25 15:15:48 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,64 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var dmemo = document.getElementsByClassName("delete")
+
+
+//Remove the list
+function removeList(event) {
+	event.target.parentNode.remove();
+}
+
+
+// the first 6 list
+for (i=0; i < dmemo.length; i++) {
+	dmemo[i].onclick = removeList;
+}
+
+
+// cross the items, two methods to pickup: ul and li
+function crossLine(event){
+	event.target.classList.toggle("done");
+}
+
+ul.addEventListener("click", crossLine);
+
+
+// create the new items
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var cbtn = document.createElement("button");
+	var cclass = document.createAttribute("class");
+	cbtn.appendChild(document.createTextNode("Delete"));
+	cclass.value ="delete"
+	cbtn.setAttributeNode(cclass);
+	cbtn.onclick= removeList;
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	li.appendChild(cbtn);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+
